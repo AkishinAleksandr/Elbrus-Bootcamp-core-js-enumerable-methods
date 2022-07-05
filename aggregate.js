e7baef5634cf3620f1e8d919693715fc915967f3
Igor Polnik <ipolnik@yahoo.com>
2022-07-06 00:59:11 +0600

    array-methods-finished

--- aggregate.js
+++ aggregate.js
@@ -1,6 +1,13 @@
-function sum(arrayNumbers, sum = 0){
+/* function sum(arrayNumbers, sum = 0){
   for (let i = 0; i < arrayNumbers.length; i++ ){
     sum += arrayNumbers[i];
   }
   return sum
+} */
+
+function sum(arrayNumbers, sum = 0){
+  sum = arrayNumbers.reduce((acc, el) => acc + el);
+  return sum;
 }
+
+console.log(sum([1, 2, 5, 7]))
--- find.js
+++ find.js
@@ -1,13 +1,27 @@
-function firstUnder(numbers, limit){
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] <= limit)
-    return numbers[i]
+// function firstUnder(numbers, limit){
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] <= limit)
+//     return numbers[i]
+//   }
+// }
+
+function getNumber (numbers, limit){
+ const num =  numbers.find((elem) => elem <= limit);
+ return num;
   }
+
+   console.log(getNumber([11, 2, 6, 10], 2))
+
+// function startsWith(strings, letter){
+//   for (let i = 0; i < strings.length; i++) {
+//     if (strings[i][0] === letter)
+//     return strings[i]
+//   }
+// }
+
+function getLetter(strings, letter){
+    const letterFound = strings.find((el, i) => el[i][0] === letter)
+    return letterFound;
 }
 
-function startsWith(strings, letter){
-  for (let i = 0; i < strings.length; i++) {
-    if (strings[i][0] === letter)
-    return strings[i]
-  }
-}
+console.log(getLetter(["ytryt", "ytuyjkk", "hjkjk"], "k"))
